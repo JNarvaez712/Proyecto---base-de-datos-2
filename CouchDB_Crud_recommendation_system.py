73472bf2a4fe5cc54262b965ef6a75bbd7b5ba39
Jonathan Steven Narvaez Navia <narvaez.jonathan0712@gmail.com>
2024-03-18 01:09:11 -0500

    segundo commit

--- CouchDB_Crud_recommendation_system.py
+++ CouchDB_Crud_recommendation_system.py
@@ -1,86 +1,115 @@
 import couchdb
 
-conn_string = "http://admin:admin@localhost:5984"
-server = couchdb.Server(conn_string)
-db_name = "recommendation_system"
+#Integrantes: 
+#David Eduardo Quiceno - 2215539
+#Jonathan Steven Narváez Navia - 2215983
 
-if db_name in server:
-    print("Conectado con la BD")
-    db = server[db_name]
-else:
-    print("---Creando base de datos")
-    db = server.create(db_name)
+# Conexión a la base de datos CouchDB
+server = couchdb.Server("http://admin:admin@localhost:5984")
+db = server["recommendation_system"]
 
-    #**** CRUD ****
-#read: select all
-id_doc = input("Ingrese el id del documento:") 
-doc = db[id_doc]
-print(doc)
+# Operaciones CRUD
 
-### Funciones del crud ###
-def Create(collection, data):
-    doc_id, doc_rev = db.save(data)
+# Creación de Usuario, Tutor o Curso
 
-def Update(doc_id, data):
-    doc = db[doc_id]
-    doc.update(data)
-    db.save(doc)
+def crear_usuario(usuario):
+        db.save(usuario)
+        
+def crear_tutor(tutor):
+        db.save(tutor)
+        
+def crear_curso(usuario):
+        db.save(usuario)
+        
+# Consultar
+        
+def curso_por_calificacion_promedio(calificacion_promedio):
+    return [doc.value for doc in db.view("cursos/por_calif", key=calificacion_promedio)]
 
-def SelectAll(collection):
-    docs = [doc for doc in db.view(f"{collection}/all")] #Sintaxis de consulta de couchDB
+def consultar_usuario_por_nombre(nombre):
+    return [doc.value for doc in db.view("usuarios/por_nombre", key=nombre)]
 
-def Select_By_Criteria(collection, criteria):
-    docs = [doc for doc in db.view(f"{collection}/by_criteria", key=criteria)]
+def consultar_tutor_por_nombre(nombre):
+    return [doc.value for doc in db.view("tutores/por_nombre", key=nombre)]
+        
+####################################
 
-def Delete(doc_id):
-    doc = db[doc_id]
-    db.delete(doc)
-
-def Delete_Value(key, value):
-    doc_idv = db.get()
-    
-
-guitarra = {
-    "id":"G001",
-    "Nombre": "Guitarra acustica",
-    "Categoria": "Musica",
-    "descripcion": "guitarra acustica basica",
-    "duracion": 40,
-    "precio": 0.0,
-    "remoto":False
+# Datos de usuario, tutor y curso
+nuevo_usuario = {
+    "id": "987654",
+    "nombre": "Juan Garcia",
+    "carrera": "Ingeniería Informática",
+    "semestre": "5",
+    "cursos_asistidos": ["2234567", "3208974"],
+    "calificaciones_curso": [{"id_curso": "2234567", "calificacion": 4.8}],
+    "calificaciones_tutor": [{"id_tutor": "2998373", "calificacion": 4.2}]
 }
 
-pintura1 = {
-    "id":"P001",
-    "Nombre": "Pintura al óleo",
-    "Categoria": "Arte",
-    "descripcion": "Pintura al óleo",
+nuevo_tutor = {
+    "id": "2298673",
+    "nombre": "Ana Martínez",
+    "carrera": "Psicología",
+    "semestre": "10",
+    "calificacionProm": 4.8,
+    "cursos_dictados": [{"id_curso": "2234567"}]
+}
+
+nuevo_curso = {
+    "id": "2234567",
+    "nombre": "Introducción a la Historia del Arte",
+    "categoria": "Artes y humanidades",
+    "modalidad": "presencial",
+    "gratuito": "F",
+    "precio": 50,
     "duracion": 30,
-    "precio": 0.0,
-    "remoto":False
+    "certificado": "V",
+    "calificacion_promedio": 4.5
 }
 
-pintura2 = {
-    "id":"P002",
-    "Nombre": "Pintura acrilica",
-    "Categoria": "Arte",
-    "descripcion": "Pintura acrilica",
-    "duracion": 50,
-    "precio": 0.0,
-    "remoto":False
-}
+#######################################################################################
 
-Create("cursos", guitarra)
-Create("cursos", pintura1)
-Create("cursos", pintura2)
+# Crear un nuevo usuario, tutor y curso
 
-### Revisar update ###
-guitarra["duracion"] = 45
-Update(guitarra)
+a = int(input("Crear:\n"
+              "1.Usuario"
+              "2.Tutor"
+              "3.Curso"))
 
-listar_cursos = SelectAll("cursos")
-for c in listar_cursos:
-    print(c)
+if (a == 1):
+    crear_usuario(nuevo_usuario)
+elif (a == 2):   
+    crear_tutor(nuevo_tutor)
+elif (a == 3):    
+    crear_curso(nuevo_curso)
+else:
+    print("Error")
 
-criterios = "Arte"
-cursos_pintura = Select_By_Criteria("cursos",criterios)
+########################################################
+
+# Consultar usuario, tutor y curso por una llave
+
+b = int(input("Consultar por llave:\n"
+              "1.Usuario (Por nombre)"
+              "2.Tutor (Por nombre)"
+              "3.Curso (Por calificación promedio)"))
+
+if (b == 1):
+    # Consultar usuario por nombre
+    usuarios_nombre = consultar_usuario_por_nombre("Juan")
+    print("Usuarios: ")
+    for usuario in usuarios_nombre:
+        print(usuario)        
+elif(b == 2):
+    # Consultar tutor por nombre
+    tutores_nombre = consultar_tutor_por_nombre("Ana")
+    print("Tutores: ")
+    for tutor in tutores_nombre:
+        print(tutor)        
+elif(b == 3):
+    # Consultar curso por calificación promedio
+    curso_calif= curso_por_calificacion_promedio(4.8)
+    print("Cursos: ")
+    for curso in curso_calif:
+        print(curso)        
+else:
+    print("Error")
